1c38c4cca1669eea42844839325dbc4e44e7da34
Madz96 <mad.subasinghe@gmail.com>
2019-08-29 11:45:09 +0530

    Added components

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MyLibComponent } from './my-lib.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ButtonComponent } from './button/button.component';
 
 
 
 @NgModule({
-  declarations: [MyLibComponent, NavbarComponent],
+  declarations: [MyLibComponent, NavbarComponent, ButtonComponent],
   imports: [
   ],
   exports: [MyLibComponent]
