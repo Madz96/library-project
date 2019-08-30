fe81696f6f67e16c0cd9da9757c76d1edb55b97e
Madz96 <mad.subasinghe@gmail.com>
2019-08-30 08:36:28 +0530

    Removed dist file from repo

--- dist/my-lib/public-api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/my-lib.service';
-export * from './lib/my-lib.component';
-export * from './lib/my-lib.module';
-export * from './lib/button/button.component';
-export * from './lib/navbar/navbar.component';
