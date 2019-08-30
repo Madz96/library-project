21ca1d81de498acc1b0b4fb6121d655272c096b8
Madz96 <mad.subasinghe@gmail.com>
2019-08-30 08:11:18 +0530

    Export components in my-lib.module.ts

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { MyLibComponent } from './my-lib.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ButtonComponent } from './button/button.component';
-
-
+import { NgModule } from "@angular/core";
+import { MyLibComponent } from "./my-lib.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ButtonComponent } from "./button/button.component";
 
 @NgModule({
   declarations: [MyLibComponent, NavbarComponent, ButtonComponent],
-  imports: [
-  ],
-  exports: [MyLibComponent]
+  imports: [],
+  exports: [MyLibComponent, NavbarComponent, ButtonComponent]
 })
-export class MyLibModule { }
+export class MyLibModule {}
