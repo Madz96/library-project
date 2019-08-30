5c306d18111936f96f3e58161c02402bdfb69e49
Madz96 <mad.subasinghe@gmail.com>
2019-08-30 08:28:39 +0530

    Changed my-lib.module

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
 import { MyLibComponent } from "./my-lib.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ButtonComponent } from "./button/button.component";
 
 @NgModule({
   declarations: [MyLibComponent, NavbarComponent, ButtonComponent],
-  imports: [],
+  imports: [CommonModule],
   exports: [MyLibComponent, NavbarComponent, ButtonComponent]
 })
 export class MyLibModule {}
